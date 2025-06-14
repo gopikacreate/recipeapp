73ee7886aed2d538ff42f939467f9f17fe25444e
gopikacreate <gopikagopakumar0799@gmail.com>
2025-06-15 03:46:05 +0530

    Initial commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,21 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Manifest for PWA/home screen */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffd86b" />
+
+        {/* Apple-specific */}
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
       <body>
         <Main />
         <NextScript />
